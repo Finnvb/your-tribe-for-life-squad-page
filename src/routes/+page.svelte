4a3da19527c897559e08cc6204da9bd5eeeff11c
Finnvb <finnvb@gmail.com>
2022-09-14 10:46:05 +0200

    prismicio setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<script>
+    import createClient from '$lib/prismicio';
+    import  * as prismicH from '@prismicio/helpers';
+    
+    const client = createClient()
+    const prismicQuery = client.getFirst()
+  </script>
+  
+  {#await prismicQuery}
+    <p>Loading...</p>
+  {:then prismicResponse}
+    <h1>{prismicH.asText(prismicResponse.data.title)}</h1>
+    {@html prismicH.asHTML(prismicResponse.data.description)}
+  {:catch error}
+    <pre>{error.message}</pre>
+  {/await}
+
+
+
+
+
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
